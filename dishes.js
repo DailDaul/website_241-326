c1731f20dab6509fa5688eb6b35a57b72bdc86d1
DailDaul <taisiyakn@gmail.com>
2025-11-27 21:13:45 +0300

    Create dishes.js

---
+++ dishes.js
@@ -0,0 +1,90 @@
+const dishes = [
+    {
+        keyword: "tom-yam",
+        name: "Том Ям с креветками",
+        price: 365,
+        category: "soup",
+        count: "350 мл",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "norwegian-soup",
+        name: "Норвежский суп",
+        price: 270,
+        category: "soup",
+        count: "400 мл",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "chicken-soup",
+        name: "Куриный суп с лапшой",
+        price: 220,
+        category: "soup",
+        count: "380 мл",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "lasagna",
+        name: "Лазанья",
+        price: 385,
+        category: "main",
+        count: "450 г",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "fried-potatoes",
+        name: "Жареная картошка с грибами",
+        price: 150,
+        category: "main",
+        count: "350 г",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "chicken-cutlets",
+        name: "Котлеты из курицы с картофельным пюре",
+        price: 225,
+        category: "main",
+        count: "420 г",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "orange-juice",
+        name: "Апельсиновый сок",
+        price: 120,
+        category: "drink",
+        count: "300 мл",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "apple-juice",
+        name: "Яблочный сок",
+        price: 90,
+        category: "drink",
+        count: "300 мл",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "carrot-juice",
+        name: "Морковный сок",
+        price: 110,
+        category: "drink",
+        count: "300 мл",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "latte",
+        name: "Кофе латте",
+        price: 180,
+        category: "drink",
+        count: "300 мл",
+        image: "https://via.placeholder.com/300x200"
+    },
+    {
+        keyword: "green-tea",
+        name: "Зеленый чай",
+        price: 90,
+        category: "drink",
+        count: "400 мл",
+        image: "https://via.placeholder.com/300x200"
+    }
+];
